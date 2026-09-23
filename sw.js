d0fdf0c514266f52960652116c1faa5c791edb62
zui512-04 <qian.zui512@gmail.com>
2026-09-23 23:03:14 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,22 +3,18 @@ const CACHE_NAME = "life-manager-v1";
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
 
     event.waitUntil(
-
         caches.open(CACHE_NAME)
             .then(cache => {
-
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
-
+                return cache.addAll(FILES_TO_CACHE);
             })
-
     );
 
 });
@@ -31,8 +27,7 @@ self.addEventListener("fetch", event => {
         caches.match(event.request)
             .then(response => {
 
-                return response ||
-                    fetch(event.request);
+                return response || fetch(event.request);
 
             })
 
@@ -52,13 +47,9 @@ self.addEventListener("activate", event => {
 
                     keys.map(key => {
 
-                        if (
-                            key !== CACHE_NAME
-                        ) {
+                        if (key !== CACHE_NAME) {
 
-                            return caches.delete(
-                                key
-                            );
+                            return caches.delete(key);
 
                         }
 
